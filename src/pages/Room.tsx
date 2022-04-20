9dfc8d4ff8ca02ddc8bd9070b1c40a94d3472c27
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-20 21:40:34 +0100

    Code Room Prop

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -8,13 +8,21 @@ import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 import '../styles/room.scss';
 
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
+  const params = useParams<RoomParams>();
+
+  const roomId = params.id;
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="OnQuestions Logo" />
-          <div>code</div>
+          <RoomCode code={roomId} />
         </div>
       </header>
       <main>
@@ -26,7 +34,10 @@ export function Room() {
         <form>
           <textarea placeholder="What do you want to ask?" />
           <div className="form-footer">
-            <span>If you want to ask something, please <button>make your login</button>.</span>
+            <span>
+              If you want to ask something, please{' '}
+              <button>make your login</button>.
+            </span>
             <Button type="submit">Submit question</Button>
           </div>
         </form>
