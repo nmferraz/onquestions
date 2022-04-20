6d81610a8005e62f9148eb538961472ad349aa2d
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-20 11:23:18 +0100

    assets + basic styles

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function Home() {
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Create Q&amp;A rooms on live</strong>
+        <p>Make questions in real time</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Create your room with Google
+          </button>
+          <div className="separator">or enter in a room</div>
+          <form>
+            <input 
+              type="text"
+              placeholder="Type the room code"
+            />
+            <Button />
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
