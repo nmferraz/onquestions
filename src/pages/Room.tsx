47221480c01f0ef024e52852baa877767d931a8a
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-20 23:26:52 +0100

    implement likes

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -46,6 +46,21 @@ export function Room() {
     setNewQuestion('');
   }
 
+  async function handleLikeQuestion(
+    questionId: string,
+    likeId: string | undefined
+  ) {
+    if (likeId) {
+      await database
+        .ref(`rooms/${roomId}/questions/${questionId}/likes/${likeId}`)
+        .remove();
+    } else {
+      await database.ref(`rooms/${roomId}/questions/${questionId}/likes`).push({
+        authorId: user?.id,
+      });
+    }
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -91,11 +106,14 @@ export function Room() {
                 author={question.author}
               >
                 <button
-                  className={`like-button`}
+                  className={`like-button ${question.likeId ? 'liked' : ''}`}
                   type="button"
                   aria-label="Mark as liked"
+                  onClick={() =>
+                    handleLikeQuestion(question.id, question.likeId)
+                  }
                 >
-                  <span>15</span>
+                  {question.likeCount > 0 && <span>{question.likeCount}</span>}
                   <svg
                     width="24"
                     height="24"
